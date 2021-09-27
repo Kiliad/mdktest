b326964532b6b1acc9553b0d7c8d5e2cdeda1eb5
Kiliad <90642440+Kiliad@users.noreply.github.com>
2021-09-27 09:02:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
- 
-$(window).scroll(function(){
-if ($(this).scrollTop() > 100) {
-$('.scrollup').fadeIn();
-} else {
-$('.scrollup').fadeOut();
-}
-});
- 
-$('.scrollup').click(function(){
-$("html, body").animate({ scrollTop: 0 }, 600);
-return false;
-});
- 
+  
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 50) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+        });
+          
+        $('.scrollup').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 1);
+        return false;
+    });
+  
 });
